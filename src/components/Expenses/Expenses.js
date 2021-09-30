69d990d8eed9f51402e2c837b0f83c21ed5f52e0
Matthew Bateman <mmbateman2013@gmail.com>
2021-09-30 15:00:44 -0500

    Completed First Course module

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,16 @@
+import React from "react";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
   const expenses = props.expenses;
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {expenses.map((e) => (
         <ExpenseItem title={e.title} amount={e.amount} date={e.date} />
       ))}
-    </div>
+    </Card>
   );
 };
 
