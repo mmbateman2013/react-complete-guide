bb40fda2b32ee9b30fa88385c16aa4ccfa4fd1a8
Matthew Bateman <mmbateman2013@gmail.com>
2021-10-01 15:46:55 -0500

    Completed up to Assignment 3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,10 +6,11 @@ import "./Expenses.css";
 
 const Expenses = (props) => {
   const [selectYear, setSelectYear] = useState("2020");
-  const expenses = props.expenses;
+
   const receiveSelectYear = (year) => {
     setSelectYear(year);
   };
+
   return (
     <div>
       <Card className="expenses">
@@ -17,8 +18,13 @@ const Expenses = (props) => {
           onSelectYear={receiveSelectYear}
           selectedYear={selectYear}
         />
-        {expenses.map((e) => (
-          <ExpenseItem title={e.title} amount={e.amount} date={e.date} />
+        {props.expenses.map((e) => (
+          <ExpenseItem
+            key={e.id}
+            title={e.title}
+            amount={e.amount}
+            date={e.date}
+          />
         ))}
       </Card>
     </div>
